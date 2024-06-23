53f605b771a9ecf168e28e6fb260f6599c379420
Gabriel P <60277324+gabrielp-oliveira@users.noreply.github.com>
2024-06-23 01:12:02 -0400

    created google signup

--- routes/routes.go
+++ routes/routes.go
@@ -1,17 +1,41 @@
 package routes
 
 import (
-	"PaperTrail-auth.com/middlewares"
+	// "PaperTrail-auth.com/middlewares"
+
+	"PaperTrail-auth.com/auth"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(server *gin.Engine) {
-	server.GET("/status", status) // GET, POST, PUT, PATCH, DELETE
+	// server.GET("/status", status) // GET, POST, PUT, PATCH, DELETE
 
-	authenticated := server.Group("/")
-	authenticated.Use(middlewares.Authenticate)
+	// authenticated := server.Group("/")
+	// authenticated.Use(middlewares.Authenticate)
 
-	server.POST("/signup", signup)
-	// server.POST("/login", login)
+	server.POST("/signup", auth.Signup)
+	server.POST("/login", auth.Login)
+
+	googleAuth := server.Group("/auth/google")
+
+	googleAuth.GET("/", handleMain)
+	googleAuth.GET("/login", auth.HandleGoogleLogin)
+	googleAuth.GET("/callback", auth.HandleGoogleCallback)
+
+	// http.HandleFunc("/", handleMain)
+	// http.HandleFunc("/auth/google/login", auth.HandleGoogleLogin)
+	// http.HandleFunc("/auth/google/callback", auth.HandleGoogleCallback)
+
+}
+
+func handleMain(c *gin.Context) {
+	var htmlIndex = `<html>
+	<body>
+	<a href="/auth/google/login">Google Log In</a>
+	</body>
+	</html>`
+
+	c.Header("Content-Type", "text/html")
+	c.String(200, htmlIndex)
 }
